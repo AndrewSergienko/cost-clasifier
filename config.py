6592da83634378fe2431fd789eb04b816e59d02f
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-01-07 17:59:19 +0200

    delete configs

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    DB_USER = "postgres"
-    DB_PASSWORD = "123456"
-    DB_NAME = "finances"
-    DB_HOST = "localhost"
-    DB_CONFIG = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
